a60250d1548f1a0f19024bd7f1ed8c4c7666e9b6
Jia Hao Goh <jiahaog@users.noreply.github.com>
2021-02-19 14:10:30 +0800

    Add a repro

--- test/dart_debugger_uncaught_exception_repro_test.dart
+++ test/dart_debugger_uncaught_exception_repro_test.dart
@@ -1,8 +1,21 @@
-import 'package:dart_debugger_uncaught_exception_repro/dart_debugger_uncaught_exception_repro.dart';
 import 'package:test/test.dart';
 
+class FooException implements Exception {}
+
+Future<void> throwFooException() async {
+  // Commenting it out makes the problem go away.
+  // Wait for the next event.
+  await Future(() {});
+  throw FooException();
+}
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('foo', () async {
+    try {
+      // Inlining [throwFooException] here also makes the problem go away.
+      await throwFooException();
+    } on FooException {
+      print('caught fooexception');
+    }
   });
 }
